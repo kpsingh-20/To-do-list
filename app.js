8c4a721974474d298db8b5107f61ad07d4817e1c
kpsingh <pkrishnapal08@gmail.com>
2022-08-18 01:59:02 +0530

    add mongoose upto deletion

--- app.js
+++ app.js
@@ -3,43 +3,87 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 
-var item = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemSchema = new mongoose.Schema({
+    name : String
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1  = new Item({
+    name : "item1"
+});
+const item2  = new Item({
+    name : "item2"
+});
+const item3  = new Item({
+    name : "item3"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+
 app.get("/", function(req, res){
-    // res.send("Hello");
-    // res.sendFile(__dirname + "/index.html");
 
-    // res.write("<p> hello this is para </p>");
-    // res.write("<h1> hello this is h1 </h1>");
-    // res.send();
+    Item.find(function(err, item){
+        if(item.length === 0){
+            Item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("success");
+                }
+            });
+            res.redirect("/");
+        }else
+            res.render("index", {kindOfDay : "Today", newItem : item});
 
-    var today = new Date();
-    var currentDay = today.getDay();
-
-    var options = {
-      year : "numeric",
-        weekday : "long",
-        day : "numeric",
-        month : "long",
-        date : "numeric",
-    };
-
-    var day = today.toLocaleString("hi-IN", options);
-    res.render("index", {kindOfDay : day, newItem : item});
+    })
 });
 
 
 app.post("/", function(req, res){
-  item.push(req.body.item);
-  // console.log(item);
+  // item.push(req.body.item);
 
-    res.redirect("/");
+  console.log(req.body);
+  // dono ki alag alag post request bana kar aasani se kiya ja sakta tha.
+
+  if(req.body.cBox === "add"){
+      let object = new Item({
+          name : req.body.item
+      })
+
+      object.save();
+      res.redirect("/");
+  }else{
+  Item.deleteOne({_id : req.body.cBox}, function(err){
+      if(err){
+          console.log(err);
+      }else{
+          console.log("successfully deleted");
+      }
+  });
+  res.redirect("/");
+}
+  // Item.insertMany([object], function(err){
+  //     if(err){
+  //         console.log(err);
+  //     }else{
+  //         console.log("success");
+  //     }
+  // })
+  // // console.log(item);
+  //
+  //   res.redirect("/");
 })
 
 app.listen(3000, function(req, res){
